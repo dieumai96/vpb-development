4169b359577f8f272b2e7bf5970b84657db49685
Vu Van Thuy <vuthuycntt1996@gmail.com>
2020-03-08 19:53:55 +0700

    update code

---
+++ src/components/BranchATM/context/BranchATM.context.js
@@ -0,0 +1,4 @@
+import { createContext } from 'react';
+const BranchATMContext = createContext({});
+
+export default BranchATMContext;
---
+++ src/components/BranchATM/context/BranchATM.reducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+  seachPayload: {}
+}
+
+const BranchATMReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
