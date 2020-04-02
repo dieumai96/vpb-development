7a8e17f16f464e2a35fa79f037b655a1126c2ce0
Vu Van Thuy <vuthuycntt1996@gmail.com>
2020-04-02 15:30:23 +0700

    add uuid lib

--- src/components/SupportNow/SupportNowMenuItem.jsx
+++ src/components/SupportNow/SupportNowMenuItem.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import * as uuid from 'uuid';
 
 const SupportNowMenuItem = ({ item, index }) => {
+  const [menuItem, setMenuItem] = useState({});
+
+  useEffect(() => {
+    setIdAutomaticMenuItem();
+  }, [item.Title]);
+
+  const setIdAutomaticMenuItem = () => {
+    let idAutomatic = uuid.v4();
+    let newItem = { ...item };
+    newItem.parentId = idAutomatic;
+    if (newItem?.TagItems?.length) {
+      newItem.TagItems.forEach(item => {
+        item.parentId = idAutomatic;
+      })
+    }
+    setMenuItem(newItem);
+  }
+
   return (
     <div className="support-categories-desktop__list__items">
       <div className="title">
         <input className="vp-checkbox parent" type="checkbox" id={'items-' + index} />
         <label htmlFor={'items-' + index}>
-          {item?.Title}
+          {menuItem?.Title}
         </label>
       </div>
       <div className="close show">
@@ -15,8 +34,8 @@ const SupportNowMenuItem = ({ item, index }) => {
         </a>
       </div>
       <div className="sub-list collapse show" id={'sub-list-' + index}>
-        {item?.TagItems?.length ? (
-          item.TagItems.map((childItem, indexChild) => (
+        {menuItem?.TagItems?.length ? (
+          menuItem.TagItems.map((childItem, indexChild) => (
             <div className="sub-list__items" key={indexChild}>
               <input className="vp-checkbox child" type="checkbox" id={'items-' + index + '-' + indexChild} />
               <label htmlFor={'items-' + index + '-' + indexChild}>
